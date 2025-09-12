02cf2348a325f7d8a129b70b22318c2bd12a5036
Nhất Thanh Trần Hồ <30461021+nhatthanh1998@users.noreply.github.com>
2025-09-12 08:58:48 +0700

    feature: add 3 solutions to calculate sum to N

---
+++ src/problem4/sumToN.ts
@@ -0,0 +1,54 @@
+/**
+ * Validates that n is a positive integer.
+ *
+ * @param n - The number to validate.
+ * @throws Error if `n` is not a positive integer.
+ */
+export function validateN(n: number): void {
+  if (!Number.isInteger(n) || n <= 0) {
+    throw new Error("n must be a positive integer!");
+  }
+}
+
+/**
+ * Solution 1 to calculate the sum of all integers from 1 up to the given positive integer `n`
+ * using a for loop.
+ *
+ * @param n - A positive integer specifying the upper bound of the summation.
+ * @returns The sum of numbers from 1 to n.
+ * @throws Error if `n` is not a positive integer.
+ */
+export function sum_to_n_a(n: number): number {
+  validateN(n);
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+/**
+ * Solution 2 to calculate the sum of all integers from 1 up to the given positive integer `n`
+ * by creating an array and calculating the sum of an array.
+ *
+ * @param n - A positive integer specifying the upper bound of the summation.
+ * @returns The sum of numbers from 1 to n.
+ * @throws Error if `n` is not a positive integer.
+ */
+export function sum_to_n_b(n: number): number {
+  validateN(n);
+  return Array.from({ length: n }, (_, i) => i + 1).reduce((a, b) => a + b);
+}
+
+/**
+ * Solution 3 to calculate the sum of all integers from 1 up to the given positive integer `n`
+ * by using math formula.
+ *
+ * @param n - A positive integer specifying the upper bound of the summation.
+ * @returns The sum of numbers from 1 to n.
+ * @throws Error if `n` is not a positive integer.
+ */
+export function sum_to_n_c(n: number): number {
+  validateN(n);
+  return (n * (n + 1)) / 2;
+}
